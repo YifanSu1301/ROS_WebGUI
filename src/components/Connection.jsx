5f349ba25968d6c585533455c0049352c63ebd39
Andrew Su <YifanSu1301>
2022-10-27 00:55:23 -0400

    :fire: ros Connection'

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -1,14 +1,57 @@
 import React, { Component } from "react";
-import ROSLIB from "roslib";
+import { Alert } from "react-bootstrap";
 
 class Connection extends Component {
-  state = {};
+  state = { connected: false, ros: null };
+
+  //   constructor() {
+  //     super();
+  //     // this.init_connection();
+  //   }
+
+  componentDidMount() {
+    this.init_connection();
+  }
 
   init_connection() {
-    var ros = new ROSLIB.Ros();
+    this.state.ros = new window.ROSLIB.Ros();
+    console.log(this.state.ros);
+
+    this.state.ros.on("connection", () => {
+      console.log("connection established");
+      this.setState({ connected: true });
+    });
+
+    this.state.ros.on("close", () => {
+      console.log("connection closed");
+      this.setState({ connected: false });
+      //Try to reconnect every 3 seconds
+      setTimeout(() => {
+        try {
+          this.state.ros.connect("ws://172.28.106.76:9090");
+        } catch (error) {
+          console.log("connection problem");
+        }
+      }, 3000);
+    });
+
+    try {
+      this.state.ros.connect("ws://172.28.106.76:9090");
+    } catch (error) {
+      console.log("connection problem");
+    }
   }
   render() {
-    return <h2>Connection</h2>;
+    return (
+      <div>
+        <Alert
+          className="text-center m-3"
+          variant={this.state.connected ? "success" : "danger"}
+        >
+          {this.state.connected ? "Robot Connected" : "Robot Disconnected"}
+        </Alert>
+      </div>
+    );
   }
 }
 
